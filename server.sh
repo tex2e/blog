744c859fae4c8d74caeb3a61b24dc9469a3653a2
Mako <mnfeconicu41@gmail.com>
2019-11-07 22:13:45 +0900

    update server.sh: build --future

--- server.sh
+++ server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$1" =~ ^build ]]; then
-  bundle exec jekyll build
+  bundle exec jekyll build --future
   exit
 fi
 
 if [[ "$1" =~ ^re ]]; then
-  bundle exec jekyll build
+  bundle exec jekyll build --future
 fi
 
 bundle exec jekyll server -I --livereload --future
