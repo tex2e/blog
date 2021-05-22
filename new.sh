9cbf1cfa1e5c5eab0b620bfd88fd966544d82a24
Mako <mnfeconicu41@gmail.com>
2021-05-22 11:47:10 +0900

    update new.sh

--- new.sh
+++ new.sh
@@ -7,6 +7,7 @@ if [[ $# -ne 2 ]]; then
 fi
 
 directory=$1
+cover="cover1"
 
 # Set category of article.
 case $1 in
@@ -20,20 +21,18 @@ case $1 in
   powershell|pwsh )
     directory="powershell"
     category="PowerShell" ;;
-  pentest-enum* )
-    directory="pentest-enumeration"
-    category="Pentest Enumeration" ;;
-  pentest-foothold )
-    category="Pentest Foothold" ;;
-  pentest-escalation )
-    category="Pentest Escalation" ;;
-  pentest-misc )
-    category="Pentest Misc" ;;
+  crypto )
+    cover="cover4"
   * )
     # Uppercase first character (e.g. python => Python)
     category="$(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1}"
     ;;
 esac
+
+if ! [[ -e "_posts/$directory" ]]; then
+  echo "Must create directory: $directory"
+  exit
+fi
 
 cat > "_posts/$directory/$(date +%Y-%m-%d)-${2%.md}.md" <<EOS
 ---
@@ -41,7 +40,7 @@ layout:        post
 title:         "This_is_Awesome"
 date:          $(date +%Y-%m-%d)
 category:      $category
-cover:         /assets/cover1.jpg
+cover:         /assets/$cover.jpg
 redirect_from:
 comments:      true
 published:     true
