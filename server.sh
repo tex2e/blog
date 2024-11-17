83d5644bd1637b7a532d7e758472fe1a6a611edf
Mako <10919093+tex2e@users.noreply.github.com>
2024-11-17 11:56:37 +0900

    update server.sh command

--- server.sh
+++ server.sh
@@ -9,16 +9,30 @@
     BUNDLE=/opt/homebrew/opt/ruby/bin/bundle
   fi
 
+  # ビルドのみ
   if [[ "${1:-}" =~ ^build ]]; then
     "$BUNDLE" exec jekyll build --future
     exit
   fi
 
+  # ビルドしてからサーバ起動
   if [[ "${1:-}" =~ ^re ]]; then
     "$BUNDLE" exec jekyll build --future
+    shift
   fi
 
-  "$BUNDLE" exec jekyll server --incremental --livereload --future
+  # Start Server
+
+  # ホットリロード高速化対応
+  EXTRA_ARGS=
+  if [ $# -ge 1 ]; then
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+      EXTRA_ARGS+="--limit_posts $1"
+      shift
+    fi
+  fi
+
+  "$BUNDLE" exec jekyll server --incremental --livereload --future $EXTRA_ARGS
   # --limit_posts 1
   # --port 4001
 }
